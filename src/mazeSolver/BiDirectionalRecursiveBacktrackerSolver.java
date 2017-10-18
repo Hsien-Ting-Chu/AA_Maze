112126bd49a6d346fafa48caa545c4e1b0e27de1
rmit-s3598781-david-huang <s3598781@student.rmit.edu.au>
2017-10-18 21:00:13 +1100

    update commenting

--- src/mazeSolver/BiDirectionalRecursiveBacktrackerSolver.java
+++ src/mazeSolver/BiDirectionalRecursiveBacktrackerSolver.java
@@ -4,22 +4,78 @@ import java.util.*;
 import maze.*;
 
 /**
- * Implements the BiDirectional recursive backtracking maze solving algorithm.
+ * Compute depth first search traversal on a graph.
+ * 
+ *  @author Ying-Chieh Huang s3598781
  */
 public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
 	
+	/** The maze. */
 	Maze maze;
-	Cell hCell;
-	Cell tCell;
+	
+	/** Is the maze solved. */
 	boolean isSolved = false;
-	int count = 0;
+	
+	/** The number of explored cells. */
+	int cellsCount = 0;
+	
+	/** The beginning cell in the head path. */
+	Cell hCell;
+	
+	/** The beginning cell in the tail path. */
+	Cell tCell;
+	
+	/** The list to store cells that head cell has been visited. */
 	ArrayList<Cell> hPath = new ArrayList<>();
+	
+	/** The list to store cells that tail cell has been visited. */
 	ArrayList<Cell> tPath = new ArrayList<>();
-	Random rand = new Random();
+	
+	/** To record the cells in the head path that were visited. */
 	boolean[][] hVisited;
+	
+	/** To record the cells in the tail path that were visited. */
 	boolean[][] tVisited;
+	
+	/** To generate a random number. */
+	Random rand = new Random();
+	
+	/** For dedicating the value from which head or tail path */
 	boolean isHead = true;
 	
+	
+	/** 
+     * Depth first search traversal of input graph maze from both entrance and exit to each other.
+     * 
+     * ******************************************************************************************
+     * 
+     * ALGORITHM DFS ( Maze )
+     * Perform a depth first search traversal of a graph.
+     * Input: Maze maze.
+     * OUTPUT : Maze maze with its cells that marked after they were visited,
+     *          and displays on a graph user interface.
+     * 
+     * 1: cVisited = [][]
+     * // mark all cells in both 2D-arrays unvisited
+     * for i = 0 to sizeR do
+     *   for j = 0 to sizeC do
+     *     hVisited[ i ][ j ] = false
+     *     tVisited[ i ][ j ] = false
+     * end for
+     * // initiate cells from the entrance and the exit of maze 
+     * 2: begin from the entrance cell / exit cell
+     * while it's not solved
+     * 3: Begin from the path which is shorter than another.
+     * 4: Move to the available neighbour's cell if there's no wall in between.
+     * 5: If it reached to a dead end, return to the previous cell which has other available
+     *    neighbour's cell to visit.
+     * 6: if one of the fronts meet each other or other's visited cells
+     * end while
+     * 
+     * ******************************************************************************************
+     * 
+     * @param maze Input maze.
+     */
 	@Override
 	public void solveMaze(Maze maze) {
 		
@@ -29,6 +85,7 @@ public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
 		hPath.add(hCell);
 		tPath.add(tCell);
 		
+		// initialise the value of both 2D-arrays with false
 		hVisited = new boolean[maze.sizeR][maze.sizeC];
 		tVisited = new boolean[maze.sizeR][maze.sizeC];
 		for(int i=0; i<maze.sizeR; i++) {
@@ -37,24 +94,31 @@ public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
 				tVisited[i][j] = false;
 			}
 		}
+		// to store the direction
 		int dir;
+		// to store the neighbour's cell
 		Cell nCell;
 		
 		while( !isSolved ) {
+			// in order to move the DFS fronts in a equal speed
 			if( hPath.size() <= tPath.size() ) {
 				isHead = true;
+				// get the direction of available neighbour's cell
 				dir = getNeib(hCell, isHead);
+				// if it's -1 means it has to backtrack to the previous cell
 				if( dir != -1 ) {
 					while( dir != -1 ) {
 						nCell = hCell.neigh[dir];
 						setVisited(nCell, isHead);
 						hPath.add(nCell);
+						// if it's approached to a tunnel, move the current cell to the other side of tunnel
 						if( nCell.tunnelTo != null  ) {
 							nCell = nCell.tunnelTo;
 							setVisited(nCell, isHead);
 							hPath.add(nCell);
 						}
 						hCell = nCell;
+						// check if the DFS fronts meet each other or not
 						if( hCell == tCell || isVisited(hCell, isHead)) {
 							isSolved = true;
 							break;
@@ -62,9 +126,11 @@ public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
 						if( hPath.size() > tPath.size() ) {
 							break;
 						}
+						// keep go through all the possible available neighbour's cell
 						dir = getNeib(hCell, isHead);
 					}
 				} else {
+					// delete the stored cell in the path during backtrack
 					hPath.remove(hPath.size()-1);
 					hCell = hPath.get(hPath.size()-1);
 					if( hCell.tunnelTo != null && getNeib(hCell, isHead) == -1 ) {
@@ -75,18 +141,22 @@ public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
 				}
 			} else {
 				isHead = false;
+				// get the direction of available neighbour's cell
 				dir = getNeib(tCell, isHead);
+				// if it's -1 means it has to backtrack to the previous cell
 				if( dir != -1 ) {
 					while( dir != -1 ) {
 						nCell = tCell.neigh[dir];
 						setVisited(nCell, isHead);
 						tPath.add(nCell);
+						// if it's approached to a tunnel, move the current cell to the other side of tunnel
 						if( nCell.tunnelTo != null  ) {
 							nCell = nCell.tunnelTo;
 							setVisited(nCell, isHead);
 							tPath.add(nCell);
 						}
 						tCell = nCell;
+						// check if the DFS fronts meet each other or not
 						if( tCell == hCell || isVisited(tCell, isHead) ) {
 							isSolved = true;
 							break;
@@ -97,6 +167,7 @@ public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
 						dir = getNeib(tCell, isHead);
 					}
 				} else {
+					// delete the stored cell in the path during backtrack
 					tPath.remove(tPath.size()-1);
 					tCell = tPath.get(tPath.size()-1);
 					if( tCell.tunnelTo != null && getNeib(tCell, isHead) == -1 ) {
@@ -111,21 +182,39 @@ public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
 	} // end of solveMaze()
 	
 	
+	/** 
+     * Return the value isSolved of the maze.
+     * 
+     */
 	@Override
 	public boolean isSolved() {
 		return isSolved;
 	} // end if isSolved()
 
-
+	
+	/** 
+     * Return the value how many cells were explored.
+     * 
+     */
 	@Override
 	public int cellsExplored() {
-		return count;
+		return cellsCount;
 	} // end of cellsExplored()
 	
 	
+	/** 
+     * Check if there's a available neighbour's cell to visit,
+     * if there is, return the direction of a random available neighbour's cell
+     * 
+     * @param cell Input a cell.
+     * @param cell Input a value dedicates that it's from the head path or not.
+     * @return dir, the direction of that neighbour's cell
+     */
 	public int getNeib(Cell cell, boolean isHead) {
 		
+		// For storing available neighbour's cell
 		ArrayList<Integer> neibDirs = new ArrayList<>();
+		// To store the neighbour's cell
 		Cell nCell;
 		for(int i=0; i<Maze.NUM_DIR; i++) {
 			if( cell.neigh[i] != null ) {
@@ -146,35 +235,52 @@ public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
 				}
 			}
 		}
+		// initailise the number -1, means no available neighbour's cell
 		int dir = -1;
 		if( neibDirs.size() > 0) {
+			// randomly choose an available neighbour's cell
 			dir = neibDirs.get( rand.nextInt(neibDirs.size()) );
 		}
 		return dir;
 	}
 	
 	
+	/** 
+     * Mark the cell was visited and add up the number of cellsCount
+     * call the drawFtPrt() to display on the GUI.
+     * 
+     * @param cell Input a cell.
+     * @param isHead Input a value dedicates that it's from the head path or not.
+     */
 	public void setVisited(Cell cell, boolean isHead) {
 		int r = cell.r;
 		int c = cell.c;
 		if(maze.type == Maze.HEX) {
 			c = c - (r + 1) / 2;
 		}
+		// if it's from the head, mark the cell in head 2D-array
 		if( isHead ) {
 			if( !hVisited[r][c] ) {
 				hVisited[r][c] = true;
-				count++;
+				cellsCount++;
 			}
 		} else {
 			if( !tVisited[r][c] ) {
 				tVisited[r][c] = true;
-				count++;
+				cellsCount++;
 			}
 		}
 		maze.drawFtPrt(cell);
 	}
 	
 	
+	/** 
+     * Check the cell is visited in the opposite 2D-array or not
+     * 
+     * @param cell Input a cell.
+     * @param cell Input a value dedicates that it's from the head path or not.
+     * @return true, if the cell has been visited
+     */
 	public boolean isVisited(Cell cell, boolean isHead) {
 		int r = cell.r;
 		int c = cell.c;
--- src/mazeSolver/WallFollowerSolver.java
+++ src/mazeSolver/WallFollowerSolver.java
@@ -4,75 +4,142 @@ import maze.Cell;
 import maze.Maze;
 
 /**
- * Implements WallFollowerSolver
+ * Compute brute force - wall follower traversal on a graph.
+ * 
+ *  @author Ying-Chieh Huang s3598781
  */
 
 public class WallFollowerSolver implements MazeSolver {
 	
+	/** The maze. */
 	Maze maze;
+	
+	/** Is the maze solved. */
 	boolean isSolved = false;
-	int count = 0;
+	
+	/** The number of explored cells. */
+	int cellsCount = 0;
+	
+	/** To record the cells were visited. */
 	boolean[][] cVisited;
+	
+	/** Turn the direction back to the specific side after each movement
+	 *  in rectangular maze	turn = 1;
+	 *  in hexagon maze	turn = 2;
+	 * */
 	int turn = 1;
 	
+	
+	/** 
+     * Brute force - wall follower traversal of input graph maze from entrance to exit.
+     * 
+     * ******************************************************************************************
+     * 
+     * ALGORITHM Wall Follower ( Maze )
+     * Perform a wall follower traversal of a graph.
+     * Input: Maze maze.
+     * OUTPUT : Maze maze with its cells that marked after they were visited,
+     *          and displays on a graph user interface.
+     * 
+     * 1: cVisited = [][]
+     * // mark all cells unvisited
+     * for i = 0 to sizeR do
+     *   for j = 0 to sizeC do
+     *     cVisited[ i ][ j ] = false
+     * end for
+     * // initiate cell from the entrance of maze 
+     * 2: begin from the entrance cell
+     * while currCell != exit
+     *    check the direction of east = 0
+     *    move to the neighbour's cell if there's no wall in between
+     *    if there's a wall, then turn left = 1
+     * 
+     * ******************************************************************************************
+     * 
+     * @param maze Input maze.
+     */
 	@Override
 	public void solveMaze(Maze maze) {
 		
 		this.maze = maze;
+		// initialise the value of 2D-array with false
 		cVisited = new boolean[maze.sizeR][maze.sizeC];
 		for(int i=0; i<maze.sizeR; i++) {
 			for(int j=0 ;j<maze.sizeC; j++) {
 				cVisited[i][j] = false;
 			}
 		}
-		
-		if(maze.type == Maze.HEX) {
-			turn = Maze.HEX;
-		}
-		
+		// if the maze type is Hexgon, then assign value: 2 to turn
+		if(maze.type == Maze.HEX) turn = 2;
+		// started from entrance
 		Cell currCell = maze.entrance;
 		setVisited(currCell);
+		// initial direction from east = 0
 		int dir = 0;
-		if( currCell.wall[dir] == null) {
+		// to avoid the null value that rectangular cell will contain
+		while( currCell.wall[dir] == null) {
 			dir++;
 		}
+		
+		
 		while( currCell != maze.exit ) {
+			// if it's approached to a tunnel, move the current cell to the other side of tunnel
 			if( currCell.tunnelTo != null ) {
 				currCell = currCell.tunnelTo;
 				setVisited(currCell);
 			}
+			// if there's no wall, move to the neighbour's cell
 			if( !currCell.wall[dir].present ){
 				currCell = currCell.neigh[dir];
 				setVisited(currCell);
+				// Turn the direction back to the specific side after each movement
 				dir = dir - turn;
 				if(dir < 0) dir = dir + Maze.NUM_DIR;
+				// to avoid the null value that rectangular cell will contain
 				while ( currCell.wall[dir] == null ) {
 					dir = dir - 1;
 					if(dir < 0) dir = dir + Maze.NUM_DIR;
 				}
+			// turn left if there's wall in between
 			} else {
 				do {
 					dir = (dir + 1)%Maze.NUM_DIR;
+				// to avoid the null value that rectangular cell will contain
 				} while ( currCell.wall[dir] == null );
 			}
 		}
+		// when cell move to the exit of maze
 		isSolved = true;
         
 	} // end of solveMaze()
     
     
+	/** 
+     * Return the value isSolved of the maze.
+     * 
+     */
 	@Override
 	public boolean isSolved() {
 		return isSolved;
 	} // end if isSolved()
     
     
+	/** 
+     * Return the value how many cells were explored.
+     * 
+     */
 	@Override
 	public int cellsExplored() {
-		return count;
+		return cellsCount;
 	} // end of cellsExplored()
 	
 	
+	/** 
+     * Mark the cell was visited and add up the number of cellsCount
+     * call the drawFtPrt() to display on the GUI.
+     * 
+     * @param cell Input a cell.
+     */
 	public void setVisited(Cell cell) {
 		int r = cell.r;
 		int c = cell.c;
@@ -81,9 +148,9 @@ public class WallFollowerSolver implements MazeSolver {
 		}
 		if( !cVisited[r][c] ) {
 			cVisited[r][c] = true;
-			count++;
+			cellsCount++;
 		}
 		maze.drawFtPrt(cell);
-	}
+	} // end of setVisited()
 
 } // end of class WallFollowerSolver
